56b160358d3399c4359519e083bd92ab06ec7331
dolgarev <o.dolgarev@gmail.com>
2016-04-30 12:03:11 +0300

    changed Meteor version

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.2.1');
   api.use('simple:json-routes@2.1.0');
   api.addFiles('jsend_error_handler.js', 'server');
 });
